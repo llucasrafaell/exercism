5e1a2a77b26353160a67da0bfdf28a7ab0552c5a
Lucas <lucasmarthins1@gmail.com>
2024-02-01 16:46:50 -0300

    Adicionando lógica para resolver os desafios

--- need-for-speed/src/main/java/NeedForSpeed.java
+++ need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,31 +1,48 @@
 class NeedForSpeed {
+
+    int speed;
+    int distanceDrive = 0;
+    private int batteryDrain;
+    private int battery = 100;
+
     NeedForSpeed(int speed, int batteryDrain) {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed constructor");
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
     }
 
     public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+        return battery < batteryDrain; 
     }
 
     public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+        return distanceDrive;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+        if (!batteryDrained()){
+            distanceDrive += speed;
+            battery -= batteryDrain;
+        }
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(50, 4);
     }
 }
 
 class RaceTrack {
+    int distance;
+
     RaceTrack(int distance) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack constructor");
+        this.distance = distance;
     }
 
     public boolean tryFinishTrack(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.tryFinishTrack() method");
+
+       while (!car.batteryDrained() && car.distanceDrive < distance){
+            car.drive();
+       }
+        
+       return car.distanceDrive >= distance;
     }
 }
--- need-for-speed/src/test/java/NeedForSpeedTest.java
+++ need-for-speed/src/test/java/NeedForSpeedTest.java
@@ -13,7 +13,7 @@ public class NeedForSpeedTest {
     public void new_remote_control_car_has_not_driven_any_distance() {
         int speed = 10;
         int batteryDrain = 2;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         assertThat(car.distanceDriven()).isEqualTo(0);
     }
@@ -24,7 +24,7 @@ public class NeedForSpeedTest {
     public void drive_increases_distance_driven_with_speed() {
         int speed = 5;
         int batteryDrain = 1;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         car.drive();
 
@@ -37,7 +37,7 @@ public class NeedForSpeedTest {
     public void drive_does_not_increase_distance_driven_when_battery_drained() {
         int speed = 9;
         int batteryDrain = 50;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         // Drain the battery
         car.drive();
@@ -55,7 +55,7 @@ public class NeedForSpeedTest {
     public void new_remote_control_car_battery_is_not_drained() {
         int speed = 15;
         int batteryDrain = 3;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         assertThat(car.batteryDrained()).isFalse();
     }
@@ -66,10 +66,10 @@ public class NeedForSpeedTest {
     public void drive_to_almost_drain_battery() {
         int speed = 2;
         int batteryDrain = 1;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         // Almost drain the battery
-        for (var i = 0; i < 99; i++) {
+        for (int i = 0; i < 99; i++) {
             car.drive();
         }
 
@@ -82,10 +82,10 @@ public class NeedForSpeedTest {
     public void drive_until_battery_is_drained() {
         int speed = 2;
         int batteryDrain = 1;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         // Drain the battery
-        for (var i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             car.drive();
         }
 
@@ -96,7 +96,7 @@ public class NeedForSpeedTest {
     @Tag("task:5")
     @DisplayName("The distanceDriven method returns 0 on a new nitro car")
     public void nitro_car_has_not_driven_any_distance() {
-        var car = NeedForSpeed.nitro();
+        NeedForSpeed car = NeedForSpeed.nitro();
         assertThat(car.distanceDriven()).isEqualTo(0);
     }
 
@@ -104,7 +104,7 @@ public class NeedForSpeedTest {
     @Tag("task:5")
     @DisplayName("The batteryDrained method returns false when nitro car never drove")
     public void nitro_car_has_battery_not_drained() {
-        var car = NeedForSpeed.nitro();
+        NeedForSpeed car = NeedForSpeed.nitro();
         assertThat(car.batteryDrained()).isFalse();
     }
 
@@ -112,7 +112,7 @@ public class NeedForSpeedTest {
     @Tag("task:5")
     @DisplayName("The distanceDriven method returns the correct distance after driving a nitro car")
     public void nitro_car_has_correct_speed() {
-        var car = NeedForSpeed.nitro();
+        NeedForSpeed car = NeedForSpeed.nitro();
         car.drive();
         assertThat(car.distanceDriven()).isEqualTo(50);
     }
@@ -121,10 +121,10 @@ public class NeedForSpeedTest {
     @Tag("task:5")
     @DisplayName("The batteryDrained method returns false when nitro battery did not completely drain after driving")
     public void nitro_has_correct_battery_drain() {
-        var car = NeedForSpeed.nitro();
+        NeedForSpeed car = NeedForSpeed.nitro();
 
         // The battery is almost drained
-        for (var i = 0; i < 24; i++) {
+        for (int i = 0; i < 24; i++) {
             car.drive();
         }
 
@@ -135,10 +135,10 @@ public class NeedForSpeedTest {
     @Tag("task:5")
     @DisplayName("The batteryDrained method returns true when nitro battery completely drained after driving")
     public void nitro_battery_completely_drains() {
-        var car = NeedForSpeed.nitro();
+        NeedForSpeed car = NeedForSpeed.nitro();
 
         // The battery is drained
-        for (var i = 0; i < 25; i++) {
+        for (int i = 0; i < 25; i++) {
             car.drive();
         }
 
@@ -151,10 +151,10 @@ public class NeedForSpeedTest {
     public void car_can_finish_with_car_that_can_easily_finish() {
         int speed = 10;
         int batteryDrain = 2;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         int distance = 100;
-        var race = new RaceTrack(distance);
+        RaceTrack race = new RaceTrack(distance);
 
         assertThat(race.tryFinishTrack(car)).isTrue();
     }
@@ -165,10 +165,10 @@ public class NeedForSpeedTest {
     public void car_can_finish_with_car_that_can_just_finish() {
         int speed = 2;
         int batteryDrain = 10;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         int distance = 20;
-        var race = new RaceTrack(distance);
+        RaceTrack race = new RaceTrack(distance);
 
         assertThat(race.tryFinishTrack(car)).isTrue();
     }
@@ -179,10 +179,10 @@ public class NeedForSpeedTest {
     public void car_can_finish_with_car_that_just_cannot_finish() {
         int speed = 3;
         int batteryDrain = 20;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         int distance = 16;
-        var race = new RaceTrack(distance);
+        RaceTrack race = new RaceTrack(distance);
 
         assertThat(race.tryFinishTrack(car)).isFalse();
     }
@@ -193,10 +193,10 @@ public class NeedForSpeedTest {
     public void car_can_finish_with_car_that_cannot_finish() {
         int speed = 1;
         int batteryDrain = 20;
-        var car = new NeedForSpeed(speed, batteryDrain);
+        NeedForSpeed car = new NeedForSpeed(speed, batteryDrain);
 
         int distance = 678;
-        var race = new RaceTrack(distance);
+        RaceTrack race = new RaceTrack(distance);
 
         assertThat(race.tryFinishTrack(car)).isFalse();
     }
