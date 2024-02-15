9ed89cf57a2ead1155a6ad3da6931200525e4500
Lucas <lucasmarthins1@gmail.com>
2024-02-15 13:15:37 -0300

    CarsAssemble: Adicionando resolução do exercício

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,37 @@
 public class CarsAssemble {
 
+    int defaultVelocity = 221;
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+        if (velocityValid(speed)){
+            return speed * defaultVelocity * successRate(speed);
+        } 
+        return 0.0;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        double carsPerMinute = (defaultVelocity * successRate(speed)) / 60 ;
+
+        if(velocityValid(speed))
+            return (int)(carsPerMinute * speed);
+        return 0;
+    }
+
+    public boolean velocityValid (int speed){
+        if (speed >=0 && speed <= 10)
+            return true;
+        else 
+            return false;
+    }
+
+    public double successRate (int speed){
+        if(speed < 5)
+            return 1.0;
+        else if(speed < 9)
+            return 0.9;
+        else if(speed < 10)
+            return 0.8;
+        else
+            return 0.77;
     }
 }
