b7638b40314ea87cb908adb5582d273d0338e56b
Lucas <lucasmarthins1@gmail.com>
2024-02-15 16:11:44 -0300

    LogLevels: Adicionando resolução para o método LogLevels.message

--- log-levels/src/main/java/LogLevels.java
+++ log-levels/src/main/java/LogLevels.java
@@ -1,7 +1,15 @@
 public class LogLevels {
-    
+
+    private final static String TRIGGER_ERROR = ":";
+
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        int preMessageError = logLine.indexOf(TRIGGER_ERROR);
+        if (preMessageError != -1) {
+            String message = logLine.substring(preMessageError+1, logLine.length()).trim();
+            return message;
+        }
+        else
+            return logLine;
     }
 
     public static String logLevel(String logLine) {
