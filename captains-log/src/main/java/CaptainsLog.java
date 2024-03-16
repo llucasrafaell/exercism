8db0b32cd69ac25917bfe8bd495918187da17c17
Lucas <lucasmarthins1@gmail.com>
2024-03-16 16:44:34 -0300

    captains-log: adicionando solução do exercício

--- captains-log/src/main/java/CaptainsLog.java
+++ captains-log/src/main/java/CaptainsLog.java
@@ -1,8 +1,11 @@
 import java.util.Random;
 
 class CaptainsLog {
-
     private static final char[] PLANET_CLASSES = new char[]{'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
+    private static final Integer MIN_VALUE_NAVE = 1000;
+    private static final Integer MAX_VALUE_NAVE = 10000;
+    private static final double MIN_VALUE_DATE = 41000.0;
+    private static final double RANGE_VALUE_DATE = 1000.0;
 
     private Random random;
 
@@ -11,14 +14,16 @@ class CaptainsLog {
     }
 
     char randomPlanetClass() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomPlanetClass() method");
+        int sorteio = random.nextInt(PLANET_CLASSES.length);
+        return PLANET_CLASSES[sorteio];
     }
 
     String randomShipRegistryNumber() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomShipRegistryNumber() method");
+        int naveNumber = MIN_VALUE_NAVE + random.nextInt(MAX_VALUE_NAVE - MIN_VALUE_NAVE);
+        return String.format("NCC-%d",naveNumber);
     }
 
     double randomStardate() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomStardate() method");
+        return MIN_VALUE_DATE + RANGE_VALUE_DATE * random.nextDouble();
     }
 }
