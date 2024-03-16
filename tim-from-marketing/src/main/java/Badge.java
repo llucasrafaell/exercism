bc16c45ad3b498e095620d5afda7025a4feb2af6
Lucas <lucasmarthins1@gmail.com>
2024-03-16 15:59:22 -0300

    tim-from-marketing: refatorando solução

--- tim-from-marketing/src/main/java/Badge.java
+++ tim-from-marketing/src/main/java/Badge.java
@@ -1,20 +1,10 @@
 class Badge {
     
     public String print(Integer id, String name, String department) {
-
-        if(id == null && department != null){
-            return (name + " - " + department.toUpperCase());
-        }
-        else if (department == null && id != null){
-            return (auxIdIdentidade(id) + " - " + name + " - " +"OWNER");
-        }
-        else if (department == null && id == null){
-            return (name + " - " +"OWNER");
-        }
-        return (auxIdIdentidade(id) + " - " + name + " - " + department.toUpperCase());
-    }
-
-    public String auxIdIdentidade(Integer id){
-        return ("["+id+"]");
+       return String.format("%s%s - %s",
+            id != null ? String.format("[%d] - ", id) : "",
+            name,
+            department != null ? department.toUpperCase() : "OWNER"
+       );
     }
 }
