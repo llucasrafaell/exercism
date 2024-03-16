c3d13b8f019c641f5ffa258e1a6c83bf538e867e
Lucas <lucasmarthins1@gmail.com>
2024-03-16 15:50:18 -0300

    tim-from-marketing: adicionando solução do exercício

--- tim-from-marketing/src/main/java/Badge.java
+++ tim-from-marketing/src/main/java/Badge.java
@@ -1,5 +1,20 @@
 class Badge {
+    
     public String print(Integer id, String name, String department) {
-        throw new UnsupportedOperationException("Please implement the Badge.print() method");
+
+        if(id == null && department != null){
+            return (name + " - " + department.toUpperCase());
+        }
+        else if (department == null && id != null){
+            return (auxIdIdentidade(id) + " - " + name + " - " +"OWNER");
+        }
+        else if (department == null && id == null){
+            return (name + " - " +"OWNER");
+        }
+        return (auxIdIdentidade(id) + " - " + name + " - " + department.toUpperCase());
+    }
+
+    public String auxIdIdentidade(Integer id){
+        return ("["+id+"]");
     }
 }
